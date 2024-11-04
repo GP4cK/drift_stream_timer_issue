9dab686dca849e448b23c02d7acca5472d23c404
GP4cK <gautier.bayzelon@gmail.com>
2024-11-04 16:56:39 +0900

    :white_check_mark: adds workaround

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,6 @@
 import 'package:drift_stream_timer_issue/database.dart';
 import 'package:drift_stream_timer_issue/main.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -10,7 +11,13 @@ void main() {
   tearDownAll(() {
     db.close();
   });
-  testWidgets('MyApp', (tester) async {
+  testWidgets('Will fail', (tester) async {
     await tester.pumpWidget(MyApp(db));
+  });
+  testWidgets('Workaround', (tester) async {
+    await tester.runAsync(() async {
+      await tester.pumpWidget(MyApp(db));
+      await tester.pumpWidget(Container());
+    });
   });
 }
